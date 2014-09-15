742282e3cc302d0bc752a1331ca55fa446a144b5
Kyle Conrad <kyle@kyleconrad.com>
2014-09-15 16:14:06 -0400

    Remove all files to base

--- client/main.js
+++
@@ -1,9 +0,0 @@
-// Primary loading and app functionality
-
-// Meteor.startup(function() {
-//   Deps.autorun(function() {
-//     // console.log('There are ' + Posts.find().count() + ' posts');
-//   });
-
-//   console.log(JSON.parse(Assets.getText('sample.json')));
-// });
--- client/views/layout.js
+++
@@ -1,7 +0,0 @@
-// Layout functionality
-
-// Template.layout.helpers({
-//   pageTitle: function() {
-//   	return Session.get('pageTitle');
-//   }
-// });
