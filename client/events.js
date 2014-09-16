2519747839e84fc223efc04fd998bc79d82f0363
Kyle Conrad <kyle@kyleconrad.com>
2014-09-16 12:14:55 -0400

    Updates to gitignore to avoid tile upload

--- client/events.js
+++ client/events.js
@@ -1,6 +1,11 @@
 Template.nav.events = {
+	// ABOUT PANEL
 	'click #about-link': function (e) {
 		$('body').toggleClass('about-panel-open');
 		e.preventDefault;
 	}
+};
+
+Template.nav.rendered = function() {
+	Meteor.widowControl();
 };
--- client/startup.js
+++ client/startup.js
@@ -19,12 +19,14 @@ Meteor.startup(function () {
 
 
 	// WIDOW CONTROL
-	widowControl();
+	Meteor.widowControl();
 });
 
 
 
-function widowControl() {
+
+
+Meteor.widowControl = function() {
 	if (windowWidth > 640) {
 		$('h1, h2, h3, li, p, figcaption').each(function() {
 		    $(this).html($(this).html().replace(/\s((?=(([^\s<>]|<[^>]*>)+))\2)\s*$/,'&nbsp;$1'));
