fc73dea15ac453f530fef84030d36720bf7dbb04
Kyle Conrad <kyle@kyleconrad.com>
2014-09-18 16:43:37 -0400

    Errors in loading

--- client/views/includes/infobox.js
+++ client/views/includes/infobox.js
@@ -1,3 +1,14 @@
-Template.infobox.rendered = function() {
-	Meteor.widowControl();
+Template.infobox.events = {
+
 };
+
+Template.infobox.helpers({
+
+});
+
+Template.infobox.rendered = function() {
+	// Meteor.widowControl();
+
+	console.log(this);
+};
+
